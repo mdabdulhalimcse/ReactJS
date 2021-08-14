6785dc0d95b42f1c57780156160aa4f5459acd27
mdabdulhalimcse <mdabdulhalimcse@gmail.com>
2021-08-14 13:48:11 +0600

    React Element Concept

--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import reactDom from 'react-dom';
+// const element = React.createElement('h1', null, 'Hello, World!');
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const index = 0;
+
+const element = (
+    <h1 className="heading" tabIndex={index}>
+        <span> Time is {new Date().toLocaleTimeString()}</span>
+        <img src="" alt="" />
+    </h1>
 );
+reactDom.render(element, document.getElementById('root'));
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// setInterval(() => {
+//     const element = (
+//         <h1 className="heading" tabIndex={index}>
+//             <span> Time is {new Date().toLocaleTimeString()}</span>
+//             <img src="" alt="" />
+//         </h1>
+//     );
+//     reactDom.render(element, document.getElementById('root'));
+// }, 1000);
