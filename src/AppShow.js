e89c7f2d5781cf7a46c9e3fe0db9a1f9160ccc4d
unknown <mdabdulhalimcse@gmail.com>
2021-12-10 21:56:38 +0600

    React higher order components

--- src/AppShow.js
+++ src/AppShow.js
@@ -1,20 +1,8 @@
 import React from 'react';
-import Bracket from './componets/composition/Bracket';
-import Emoji from './componets/composition/Emoji';
-import Text from './componets/composition/Text';
 
 function AppShow(){
     return(
-//{/* <Emoji>{({ addEmoji }) => <Text addEmoji={addEmoji} />}</Emoji> */}
-<Emoji>
-    {({addEmoji}) =>(
-        <Bracket>
-        {({addBracket}) => <Text addEmoji={addEmoji} addBracket={addBracket} />}
-        </Bracket>
-    )
-
-    }
-</Emoji>
+<di>Hello</di>
     );
 }
 export default AppShow;
