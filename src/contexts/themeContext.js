6eefa022e48ddb4c32b4495df0121665a2359247
unknown <mdabdulhalimcse@gmail.com>
2021-12-14 22:18:55 +0600

    context api

--- src/contexts/themeContext.js
+++ src/contexts/themeContext.js
@@ -2,6 +2,9 @@ import createContext from "../lib/Context";
 // import React from "react";
 
 const themeContext = createContext();
+// const themeContext = createContext({
+//     theme:'blue'
+// });
 // const themeContext = React.createContext();
 
 export default themeContext;
