068816bd3e19bb4741087d3123b70f0b0abbc840
unknown <mdabdulhalimcse@gmail.com>
2021-12-18 15:47:15 +0600

    context api contextType use

--- src/contexts/themeContext.js
+++ src/contexts/themeContext.js
@@ -1,13 +1,13 @@
-import createContext from "../lib/Context";
-// import React from "react";
+// import createContext from "../lib/Context";
+import React from "react";
 
-const themeContext = createContext();
+// const themeContext = createContext();
 
 // const themeContext = createContext({
 //     theme:'blue'
 // });
 
 
-// const themeContext = React.createContext();
+const themeContext = React.createContext();
 
 export default themeContext;
