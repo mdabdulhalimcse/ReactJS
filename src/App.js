f6cadc42084bcdf26da94840367c49055dd4fad8
unknown <mdabdulhalimcse@gmail.com>
2021-12-18 22:46:23 +0600

    useState Hook use

--- src/App.js
+++ src/App.js
@@ -1,46 +1,13 @@
 import React from "react";
 import './App.css';
-import ClickCounter from "./components/ClickCounter";
-import Counter from "./components/Counter";
-import Section from "./components/Section";
-import ThemeContext from './contexts/themeContext';
 
 export default class AppShowRenderProps extends React.Component{
-    state = {
-        theme: 'light'
-    }
-    switchTheme = () =>{
-      this.setState(({theme}) => {
-        if( theme === 'dark'){
-          return {
-            theme : 'light',
-          }
-        }else{
-          return {
-            theme : 'dark',
-          }
-        }
-      })
-    }
+    
  
     render(){
-        const { theme } = this.state;
+       
         return(
-            // <div>
-            //     <Counter render={(count, increamentCount) =>  (<ClickCounter count = {count} increamentCount={increamentCount} /> )} />
-               
-            //     <Counter render={(count, increamentCount) =>  (<HoverCounter count = {count} increamentCount={increamentCount} /> )} />
-            // </div>
-    
-            <div>
-                  <Counter >
-                {(count, increamentCount) =>  (<ClickCounter count = {count} increamentCount={increamentCount} /> )}
-                </Counter>
-                <ThemeContext.Provider value={{theme , switchTheme: this.switchTheme}}>
-                           <Section  />
-                  </ThemeContext.Provider>
-            </div>
-    
-        )
+          <div>Hello</div>
+        );
     }
 }
