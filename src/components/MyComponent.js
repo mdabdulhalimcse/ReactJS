fd53cec57b2c43a07cc9eb5d1a8392df6dbcd670
unknown <mdabdulhalimcse@gmail.com>
2022-01-05 23:17:11 +0600

    useEffect hook dependecy set

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -2,16 +2,12 @@ import { useEffect, useState } from "react";
 
 export default function MyComponent (){
     const [count, setCount] = useState(0);
-    const [date, setDate] = useState(new Date());
-
-    const tick = () =>{
-        setDate(new Date());
-    }
+   const [text,setText] = useState('');
 
     useEffect(() =>{
         console.log('useEffect count is called or render');
         document.title = `Clicked ${count} times`;
-    })
+    },[count])
 
     const addClick = () => {
         setCount((prevState) => prevState + 1 );
@@ -19,7 +15,9 @@ export default function MyComponent (){
     
     return(
         <div>
-            <p>Time: {date.toLocaleTimeString("bn-BD")}</p>
+        <p> 
+            <input type="text" onChange={(e)  => setText(e.target.value)} />
+        </p>
             <p>
                 <button type="button" onClick={addClick}>Click</button>
             </p>
