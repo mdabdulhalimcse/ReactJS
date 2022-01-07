1b62c0fddd6f5d7f950bfe9ba1cbb88f24a8ee9b
unknown <mdabdulhalimcse@gmail.com>
2022-01-07 22:44:42 +0600

    React.memo instead of shouldComponentUpdate

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useCallback, useMemo, useState } from "react";
+import React, { useState } from "react";
 import Button from "./components/Button";
 import ShowCount from "./components/ShowCount";
 import Title from "./components/Title";
@@ -7,27 +7,18 @@ export default function App(){
     const [count,setCount] = useState(0); 
     const [count2,setCount2] = useState(0); 
 
-    const addOne = useCallback(() => {
-      console.log('render Function addOne');
-      setCount((prevState) => prevState + 1);
-  },[])
+    const addOne = () => {
+        setCount((prevState) => prevState + 1);
+    }
 
-    const addFive = useCallback(() => {
-      console.log('render Function addFive');
-      setCount2((prevState) => prevState + 5);
-  },[])
-
-  const isEvenOrOdd = useMemo(() =>{
-    let i = 0;
-    while( i < 900000000) i++;
-    return count % 2 === 0;
-  },[count]);
+    const addFive = () => {
+        setCount2((prevState) => prevState + 5);
+    }
 
         return(
           <div>
             <Title />
             <ShowCount title="Counter 1" count={count} />
-            <span>{isEvenOrOdd ? 'Even' : 'Odd'}</span>
             <Button handleClick={addOne}>Click</Button>
 <hr/>
             <ShowCount title="Counter 2" count={count2} />
--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 function Button ({handleClick, children}){
-    console.log('render Button component');
+    console.log(`render Button component by : ${children}`);
     return(
         <p>
             <button type="button" onClick={handleClick} >
