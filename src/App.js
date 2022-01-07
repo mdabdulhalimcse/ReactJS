22f661187ed9af05c6362fe5ae799652528f035a
unknown <mdabdulhalimcse@gmail.com>
2022-01-07 22:36:52 +0600

    useMemo hook

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useCallback, useState } from "react";
+import React, { useCallback, useMemo, useState } from "react";
 import Button from "./components/Button";
 import ShowCount from "./components/ShowCount";
 import Title from "./components/Title";
@@ -17,10 +17,17 @@ export default function App(){
       setCount2((prevState) => prevState + 5);
   },[])
 
+  const isEvenOrOdd = useMemo(() =>{
+    let i = 0;
+    while( i < 900000000) i++;
+    return count % 2 === 0;
+  },[count]);
+
         return(
           <div>
             <Title />
             <ShowCount title="Counter 1" count={count} />
+            <span>{isEvenOrOdd ? 'Even' : 'Odd'}</span>
             <Button handleClick={addOne}>Click</Button>
 <hr/>
             <ShowCount title="Counter 2" count={count2} />
