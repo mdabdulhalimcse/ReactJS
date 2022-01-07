c05d4a1122f4b86836ea3ed79a8a8f87e37877ff
unknown <mdabdulhalimcse@gmail.com>
2022-01-07 22:49:51 +0600

    useMemo hook

--- src/components/ShowCount.js
+++ src/components/ShowCount.js
@@ -1,8 +1,11 @@
+import React from "react";
+
 function ShowCount({title,count}){
+    console.log(`rendering showCount componet count is : ${count}`)
     return(
         <p>
             {title} is {count}
         </p>
     )
 }
-export default ShowCount;
+export default React.memo(ShowCount);
