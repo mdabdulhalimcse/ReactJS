b75ce94845f71a0ba5279ec8edb03f3d8e7ad7a2
unknown <mdabdulhalimcse@gmail.com>
2022-01-11 02:31:03 +0600

    useRef alternative used

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import React from "react";
-import Form from "./components/Form";
+
+import Time from "./components/Time";
 
 export default function App(){
+ 
+
+
         return(
           <div>
-                <Form />
+                <Time />
           </div>
         );
 }
