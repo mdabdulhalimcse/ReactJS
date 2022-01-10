88411ba6ea85b27d9c644beb7b6c51454b7bb4a1
unknown <mdabdulhalimcse@gmail.com>
2022-01-11 00:29:04 +0600

    useRef hook simple example

--- src/App.js
+++ src/App.js
@@ -1,35 +1,10 @@
-import React, { useCallback, useMemo, useState } from "react";
-import Button from "./components/Button";
-import ShowCount from "./components/ShowCount";
-import Title from "./components/Title";
+import React from "react";
+import Form from "./components/Form";
 
 export default function App(){
-    const [count,setCount] = useState(0); 
-    const [count2,setCount2] = useState(0); 
-
-    const addOne = useCallback(() => {
-      setCount((prevState) => prevState + 1);
-  },[])
-
-    const addFive = useCallback(() => {
-      setCount2((prevState) => prevState + 5);
-  },[])
-
-  const isEvenOrOdd = useMemo(() =>{
-    let i = 0;
-    while( i < 900000000) i++;
-    return count % 2 === 0;
-  },[count]);
-
         return(
           <div>
-            <Title />
-            <ShowCount title="Counter 1" count={count} />
-            <span>{isEvenOrOdd ? 'Even' : 'Odd'}</span>
-            <Button handleClick={addOne}>Click</Button>
-<hr/>
-            <ShowCount title="Counter 2" count={count2} />
-            <Button handleClick={addFive}>Click+5</Button>
+                <Form />
           </div>
         );
 }
--- src/components/Title.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function Title(){
-    console.log('render Title component');
-    return <h1>Welcome to react Callback Hook Tutorial</h1>
-}
-export default React.memo(Title);
